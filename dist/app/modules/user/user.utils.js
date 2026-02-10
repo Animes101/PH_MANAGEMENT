b7bb16803ba909d2687f3f01b6fb7ee8140db9d5
Animes <animesbarman101@gmail.com>
2026-02-10 17:34:07 +0600

    some change

--- dist/app/modules/user/user.utils.js
+++ dist/app/modules/user/user.utils.js
@@ -3,25 +3,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.generatedId = void 0;
 const user_model_1 = require("./user.model");
 const findLastStudentId = async () => {
-    const lastStudent = await user_model_1.UserModel.findOne({ role: 'student' }, { id: 1 }).sort({ createdAt: -1 }).lean();
-    return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+    const lastStudent = await user_model_1.UserModel
+        .findOne({ role: 'student' }, { id: 1 })
+        .sort({ createdAt: -1 })
+        .lean();
+    return lastStudent?.id;
 };
-// export const generatedId = async (payload: IAcademicSemister) => {
-//   const year = payload.year;
-//   const code = payload.code;
-//   const currentId = await findLastStudentId() || (0).toString();
-//   const incrementedId = (currentId + 1)
-//     .toString()
-//     .padStart(6, '0');
-//   const generatedId = `${year}${code}${incrementedId}`;
-//   return generatedId;
-// };
 const generatedId = async (payload) => {
-    const year = payload.year;
-    const code = payload.code;
+    const { year, code } = payload;
     const lastStudentId = await findLastStudentId();
-    const currentIdCount = lastStudentId ? parseInt(lastStudentId) : 0;
-    const incrementedId = (currentIdCount + 1).toString().padStart(4, '0');
+    let currentIdCount = 0;
+    if (lastStudentId) {
+        const lastStudentYear = lastStudentId.substring(0, 4);
+        const lastStudentCode = lastStudentId.substring(4, 6);
+        if (lastStudentYear === year.toString() &&
+            lastStudentCode === code) {
+            currentIdCount = Number(lastStudentId.substring(6));
+        }
+    }
+    const incrementedId = (currentIdCount + 1)
+        .toString()
+        .padStart(4, '0');
     const generatedId = `${year}${code}${incrementedId}`;
     return generatedId;
 };
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -2,41 +2,41 @@ import { IAcademicSemister } from "../acadamicSemister/seminter.interface";
 import { UserModel } from "./user.model";
 
 
-const findLastStudentId= async()=>{
+const findLastStudentId = async () => {
+  const lastStudent = await UserModel
+    .findOne({ role: 'student' }, { id: 1 })
+    .sort({ createdAt: -1 })
+    .lean();
 
-  const lastStudent= await UserModel.findOne({role:'student'},{id:1}).sort({createdAt:-1}).lean();
-
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
-
-}
+  return lastStudent?.id;
+};
 
 
-
-// export const generatedId = async (payload: IAcademicSemister) => {
-
-//   const year = payload.year;
-//   const code = payload.code;
-//   const currentId = await findLastStudentId() || (0).toString();
-
-//   const incrementedId = (currentId + 1)
-//     .toString()
-//     .padStart(6, '0');
-
-//   const generatedId = `${year}${code}${incrementedId}`;
-
-//   return generatedId;
-// };
 
 export const generatedId = async (payload: IAcademicSemister) => {
-  const year = payload.year;
-  const code = payload.code;
-  const lastStudentId = await findLastStudentId(); 
-  
-  const currentIdCount = lastStudentId ? parseInt(lastStudentId) : 0;
+  const { year, code } = payload;
 
-  const incrementedId = (currentIdCount + 1).toString().padStart(4, '0'); 
+  const lastStudentId = await findLastStudentId();
+
+  let currentIdCount = 0; 
+
+  if (lastStudentId) {
+    const lastStudentYear = lastStudentId.substring(0, 4);
+    const lastStudentCode = lastStudentId.substring(4, 6);
+
+    if (
+      lastStudentYear === year.toString() &&
+      lastStudentCode === code
+    ) {
+      currentIdCount = Number(lastStudentId.substring(6));
+    }
+  }
+  const incrementedId = (currentIdCount + 1)
+    .toString()
+    .padStart(4, '0');
 
   const generatedId = `${year}${code}${incrementedId}`;
 
   return generatedId;
+
 };
