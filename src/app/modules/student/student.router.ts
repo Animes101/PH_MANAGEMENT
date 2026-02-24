7f66f6865539d5eb0a9fd8fbce7423427639c1a0
Animes <animesbarman101@gmail.com>
2026-02-24 20:15:54 +0600

    add some

--- src/app/modules/student/student.router.ts
+++ src/app/modules/student/student.router.ts
@@ -1,3 +1,20 @@
+// import express from 'express';
+// import { studentController } from './student.controller';
+// import validationRequest from '../../utility/validatonJoi';
+// import { updateStudentSchema } from './student.validation';
+
+// const router= express.Router();
+
+
+
+// router.get('/getStudent', studentController.getAllStudents);
+// router.get('/singleStudent/:_id', studentController.getSingleStudent);
+// router.delete('/deleteStudent/:id', studentController.deleteStudent);
+// router.patch('/update/:id', validationRequest(updateStudentSchema), studentController.updateStudent);
+
+
+// export const StudentRoutes=router;
+
 import express from 'express';
 import { studentController } from './student.controller';
 import validationRequest from '../../utility/validatonJoi';
